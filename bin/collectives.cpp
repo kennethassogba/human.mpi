eea4ef2d48385b1c1725dc17fbcb8b8f93ebb3b1
Kenn <kennethassogba@gmail.com>
2023-04-05 21:18:02 +0200

    Add collectives tests

---
+++ bin/collectives.cpp
@@ -0,0 +1,27 @@
+// A simple example using human.mpi
+// Use the cmake(readme) or
+// mpiccx this.cpp -o this
+// mpirun -n 2 this
+#include "../inc/mpi.hpp"
+
+int main(int argc, char *argv[])
+{
+  human::mpi::communicator world(argc, argv);
+
+  auto rank = world.rank();
+  auto size = world.size();
+
+  std::cout << "Process " << rank << "/" << size << std::endl;
+
+  std::string msg;
+  if (world.rank() == world.root())
+    msg = "Hello";
+
+  bcast(msg);
+
+  std::cout << "P" << rank << " " << msg << std::endl;
+
+  world.display();
+
+  return 0;
+}
